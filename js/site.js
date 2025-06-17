4a89370478f0798588c4646f188c414d648ff4ca
Higara33 <Higara33@users.noreply.github.com>
2025-06-17 22:02:50 +0000

    deploy: a06d25b4b8b2b917c4c771bc96b3e4e7596b6f19

---
+++ js/site.js
@@ -0,0 +1,20 @@
+﻿window.scrollToSection = (id) => {
+    const el = document.getElementById(id);
+    if (el) {
+        el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+};
+
+// Запускает Bootstrap-карусель по её id
+window.startCarousel = (carouselId) => {
+    const el = document.getElementById(carouselId);
+    if (el && window.bootstrap && bootstrap.Carousel) {
+        // Инициализируем заново с теми же опциями
+        new bootstrap.Carousel(el, {
+            interval: 5000,
+            ride: 'carousel',
+            pause: false,
+            wrap: true
+        });
+    }
+};
